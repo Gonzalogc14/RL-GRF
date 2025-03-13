3d93705bf87cca0b1013ab845b6961e1d28e0cd5
Gonzalo <gonzalo02gc@gmail.com>
2025-03-13 20:59:00 +0100

    Results

--- Utils/action_selection.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-
-def epsilon_greedy_action(q_table, state, action_space, epsilon):
-    if np.random.rand() < epsilon:
-        return action_space.sample()
-    return np.argmax(q_table.get(state, np.zeros(action_space.n)))
